5ebcde0c47180a813c0b9dcc46dfa8acdd850e3c
Nicolas Parimeros <nicolas.parimeros@gmail.com>
2014-11-17 16:27:52 +0100

    on part mais ca marche presque (ou pas)

---
+++ jTrenno-ejb/ejbModule/com/cpe/jee/service/test.java
@@ -0,0 +1,11 @@
+package com.cpe.jee.service;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeManager em = new EmployeeManager();
+		System.out.println(em.findAll());
+	}
+
+}
