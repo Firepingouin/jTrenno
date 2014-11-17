4baf24b3a2c7bf8eb718c9c6fcd77b58a75bc255
Nicolas Parimeros <nicolas.parimeros@gmail.com>
2014-11-17 14:41:48 +0100

    facade employee

--- jTrenno-ejb/ejbModule/com/cpe/jee/facade/EmployeeManagerLocal.java
+++ jTrenno-ejb/ejbModule/com/cpe/jee/facade/EmployeeManagerLocal.java
@@ -1,8 +1,17 @@
 package com.cpe.jee.facade;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import com.irc.jee.model.Employee;
 
 @Local
 public interface EmployeeManagerLocal {
-
+	
+	public abstract void save (Employee e);
+	public abstract Employee update (Employee e);
+	public abstract void delete (Employee e);
+	public abstract Employee find (int employeeID);	
+	public abstract List<Employee> findAll();
 }
--- jTrenno-ejb/ejbModule/com/cpe/jee/service/EmployeeManager.java
+++ jTrenno-ejb/ejbModule/com/cpe/jee/service/EmployeeManager.java
@@ -1,9 +1,13 @@
 package com.cpe.jee.service;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
 import com.cpe.jee.facade.EmployeeManagerLocal;
+import com.irc.jee.model.Employee;
 
 /**
  * Session Bean implementation class EmployeeManager
@@ -11,6 +15,9 @@ import com.cpe.jee.facade.EmployeeManagerLocal;
 @Stateless
 @LocalBean
 public class EmployeeManager implements EmployeeManagerLocal {
+	
+	@EJB
+	private EmployeeDAO employeeDAO;
 
     /**
      * Default constructor. 
@@ -18,5 +25,35 @@ public class EmployeeManager implements EmployeeManagerLocal {
     public EmployeeManager() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public void save(Employee e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Employee update(Employee e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Employee e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Employee find(int employeeID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
