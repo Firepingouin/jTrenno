f11887095f1b81682be2ee7216c8fb4fc16ec06b
Nicolas Parimeros <nicolas.parimeros@gmail.com>
2014-11-21 02:54:52 +0100

    Project manager EJB pour ajout en base

--- jTrenno-ejb/ejbModule/com/cpe/jee/facade/ProjectManagerLocal.java
+++ jTrenno-ejb/ejbModule/com/cpe/jee/facade/ProjectManagerLocal.java
@@ -1,8 +1,12 @@
 package com.cpe.jee.facade;
 
+import java.util.Date;
+
 import javax.ejb.Local;
 
 @Local
 public interface ProjectManagerLocal {
+	
+	public abstract void createProject(float budget, String description, String name);
 
 }
