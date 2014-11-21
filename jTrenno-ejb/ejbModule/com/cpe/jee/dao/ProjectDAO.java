d3d157dcdbd943deef6f9b263c97262722459afb
DamienCPE <dami184@live.fr>
2014-11-21 10:54:08 +0100

    fixe issu projectDAO

--- jTrenno-ejb/ejbModule/com/cpe/jee/dao/ProjectDAO.java
+++ jTrenno-ejb/ejbModule/com/cpe/jee/dao/ProjectDAO.java
@@ -1,7 +1,11 @@
 package com.cpe.jee.dao;
 
+import javax.ejb.Stateless;
+
 import com.irc.jee.model.Project;
 
+
+@Stateless
 public class ProjectDAO  extends GenericDAO<Project> {
 
 	 public ProjectDAO() {
